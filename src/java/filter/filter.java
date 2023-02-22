f50a9bf1163302db5bc00f9fe0f62868e8edf4fe
VuBui6667 <vubui6667@gmail.com>
2023-02-22 23:33:41 +0700

    upload code

--- src/java/filter/filter.java
+++ src/java/filter/filter.java
@@ -116,13 +116,13 @@ public class filter implements Filter {
         user = (User) ses.getAttribute("user");
         String url = req.getServletPath();
 
-        if (user == null) {
-            if (!url.contains("login") && !url.contains("register")
-                    && !url.contains("search") && !url.contains("homeLogin")
-                    && !url.contains("flashCards")) {
-                res.sendRedirect("login.jsp");
-            }
-        }
+//        if (user == null) {
+//            if (!url.contains("login") && !url.contains("register")
+//                    && !url.contains("search") && !url.contains("homeLogin")
+//                    && !url.contains("flashCards")) {
+//                res.sendRedirect("login.jsp");
+//            }
+//        }
         
 //        if (user == null) {
 //            if (!url.contains("login") && !url.contains("register")
