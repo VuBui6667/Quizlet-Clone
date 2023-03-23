e9e09ebeca282c0866006ac35bdcd06601e33804
VuBui6667 <91791875+VuBui6667@users.noreply.github.com>
2023-03-23 21:34:59 +0700

    Update DBContext.java

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -11,11 +11,12 @@ public class DBContext {
     {
         try {
             // Edit URL , username, password to authenticate with your MS SQL Server
-            String url = "jdbc:sqlserver://localhost:1433;databaseName= Quizlett1";
-            String username = "sa";
-            String password = "123456";
+//             String url = "jdbc:sqlserver://localhost:1433;databaseName= Quizlett1";
+//             String username = "sa";
+//             String password = "123456";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            connection = DriverManager.getConnection(url, username, password);
+            String sql = "jdbc:sqlserver://quizlet.database.windows.net:1433;database=Quizlet;user=quizlet@quizlet;password=loner@@123;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+            connection = DriverManager.getConnection(sql);
         } catch (ClassNotFoundException | SQLException ex) {
             System.out.println(ex);
         }
